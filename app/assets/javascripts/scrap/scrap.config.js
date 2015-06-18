58e7fbf6fecc6c73e91c9afa2fe26627338ee41d
William Cunningham <w.a.cunningham.ii@gmail.com>
2015-06-17 23:36:25 -0400

    add syntax highlighting

--- app/assets/javascripts/scrap/scrap.config.js
+++ app/assets/javascripts/scrap/scrap.config.js
@@ -3,10 +3,16 @@
     .module('app.scrap')
     .config(config);
 
-  config.$inject = ['$httpProvider'];
+  config.$inject = ['$httpProvider', 'markedProvider'];
 
-  function config ($httpProvider) {
+  function config ($httpProvider, markedProvider) {
     var token = $('meta[name=csrf-token]').attr('content');
     $httpProvider.defaults.headers.common['X-CSRF-Token'] = token;
+
+    markedProvider.setOptions({
+      highlight: function (code) {
+        return hljs.highlightAuto(code).value;
+      }
+    });
   }
 })();
