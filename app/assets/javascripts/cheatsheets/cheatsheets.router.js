e2fa25e69241c7cc1da87e75a7e2b4ee2f8e979e
William Cunningham <w.a.cunningham.ii@gmail.com>
2015-06-22 21:35:58 -0400

    declare min-safe dependencies in cheatsheets router

--- app/assets/javascripts/cheatsheets/cheatsheets.router.js
+++ app/assets/javascripts/cheatsheets/cheatsheets.router.js
@@ -10,27 +10,27 @@ function config ($routeProvider) {
       templateUrl: 'cheatsheet.index.html',
       controller: 'CheatSheetsCtrl',
       resolve: {
-        cheatsheets: function (Cheatsheet) {
+        cheatsheets: ['Cheatsheet', function (Cheatsheet) {
           return Cheatsheet.all();
-        }
+        }]
       }
     })
     .when('/show/:id', {
       templateUrl: 'cheatsheet.show.html',
       controller: 'CheatSheetCtrl',
       resolve: {
-        cheatsheet: function ($route, Cheatsheet) {
+        cheatsheet: ['$route', 'Cheatsheet', function ($route, Cheatsheet) {
           return Cheatsheet.find($route.current.params.id);
-        }
+        }]
       }
     })
     .when('/edit/:id', {
       templateUrl: 'cheatsheet.edit.html',
       controller: 'CheatSheetEditCtrl',
       resolve: {
-        cheatsheet: function ($route, Cheatsheet) {
+        cheatsheet: ['$route', 'Cheatsheet', function ($route, Cheatsheet) {
           return Cheatsheet.find($route.current.params.id);
-        }
+        }]
       }
     })
     .otherwise('/');
